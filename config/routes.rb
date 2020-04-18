5612f77734355a8b8a959c2094b38fd6b05b4424
asdfyy <58731446+asdfyy@users.noreply.github.com>
2020-04-18 21:18:28 +0900

    z

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'addresses', to: 'users/registrations#new2'
     post 'addresses', to: 'users/registrations#create_address'
-    # post 'addresses', to: 'users/registrations#create_address'
   end
 
   root "top_page#index"
