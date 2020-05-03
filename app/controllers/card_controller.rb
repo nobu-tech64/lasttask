09abbf53a24c3b9328a2da533e97f0d9a4c607f6
asdfyy <58731446+asdfyy@users.noreply.github.com>
2020-05-03 15:21:47 +0900

    pay.jpとりあえず・・・

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -18,7 +18,9 @@ class CardController < ApplicationController
       ) #念の為metadataにuser_idを入れましたがなくてもOK
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to action: "show"
+        # redirect_to action: "show"
+        redirect_to controller: '/top_page', action: 'index'
+
       else
         redirect_to action: "pay"
       end
@@ -29,12 +31,12 @@ class CardController < ApplicationController
     card = Card.where(user_id: current_user.id).first
     if card.blank?
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = "sk_test_1e0139507a3138d861f93ed5"
       customer = Payjp::Customer.retrieve(card.customer_id)
       customer.delete
       card.delete
     end
-      redirect_to action: "new"
+      redirect_to payment_path
   end
 
   def show #Cardのデータpayjpに送り情報を取り出します
