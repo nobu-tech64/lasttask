e4976eb54e7c203d46bcb1541e9238f1b497b245
nobu-tech64 <douji533rikou@icloud.com>
2020-05-04 18:07:56 +0900

    画像がないと出品不可

--- db/migrate/20200417131622_create_images.rb
+++ db/migrate/20200417131622_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[5.0]
   def change
     create_table :images do |t|
       t.string :image
-      t.references :product, foreign_key: true
+      t.references :product, null: false, foreign_key: true
       t.timestamps
     end
   end
